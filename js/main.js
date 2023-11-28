34f72fbd156b159242448f5ae2de4a6e8805d013
enricoforesta <enrico.foresta97@gmail.com>
2023-11-28 15:02:13 +0100

    feat: milestone 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 "use strict"
+
+const { createApp } = Vue;
+
+const myApp = createApp({
+    data() {
+        return {
+            todos:[
+                {
+                    text: "Fare la spesa",
+                    done: true
+                },                {
+                    text: "Pulire casa",
+                    done: true
+                },                {
+                    text: "Studiare",
+                    done: true
+                },
+            ]
+        }
+    }
+});
+myApp.mount("#app")
